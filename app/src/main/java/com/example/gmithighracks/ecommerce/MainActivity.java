02f692a27c60b4456ec98668b4befb7885152673
gmithighracks <gmithighracks@hotmail.com>
2015-07-16 18:52:39 +0300

    test2

--- app/src/main/java/com/example/gmithighracks/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/gmithighracks/ecommerce/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //test
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

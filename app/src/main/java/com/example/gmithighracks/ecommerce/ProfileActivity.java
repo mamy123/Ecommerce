c3e297c6ac650ece3bda5e980c8b0e9f572653b4
gmithighracks <gmithighracks@hotmail.com>
2015-09-17 15:01:01 +0300

    profile first

---
+++ app/src/main/java/com/example/gmithighracks/ecommerce/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.gmithighracks.ecommerce;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+
+
+public class ProfileActivity extends  Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
--- app/src/main/java/com/example/gmithighracks/ecommerce/app/AppConfig.java
+++ app/src/main/java/com/example/gmithighracks/ecommerce/app/AppConfig.java
@@ -9,4 +9,10 @@ public class AppConfig {
 
     // Server user register url
     public static String URL_REGISTER = "http://195.134.67.152/ECommerce2/ECommerce2/index.php/register";
+
+    // Server employee profile url
+    public static String URL_PROFILE_EMPLOYEE = "http://195.134.67.152/ECommerce2/ECommerce2/index.php/login/getemployeeprofile";
+
+    // Server employer profile url
+    public static String URL_PROFILE_EMPLOYER = "http://195.134.67.152/ECommerce2/ECommerce2/index.php/login/getemployerprofile";
 }

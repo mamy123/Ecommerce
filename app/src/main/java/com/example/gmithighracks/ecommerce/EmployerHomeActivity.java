96d951f768a7489beb97e557b9c680d111868642
gmithighracks <gmithighracks@hotmail.com>
2015-09-17 17:49:23 +0300

    profile employer first

--- app/src/main/java/com/example/gmithighracks/ecommerce/EmployerHomeActivity.java
+++ app/src/main/java/com/example/gmithighracks/ecommerce/EmployerHomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.gmithighracks.ecommerce;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.gmithighracks.ecommerce.helper.SessionManager;
+
+import java.util.HashMap;
 
 
 public class EmployerHomeActivity extends ActionBarActivity {
@@ -12,6 +18,16 @@ public class EmployerHomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_home);
+        Resources res = getResources();
+        // db = new SQLiteHelper(getApplicationContext());
+
+        // session manager
+        SessionManager session = new SessionManager(getApplicationContext());
+        HashMap<String, String> user = session.getUserDetails();
+        //  String text = String.format(res.getString(R.string.employeeName), user.get("firstName"));
+      //  TextView lastMsg = (TextView)findViewById(R.id.textView2);
+        TextView lastMsg = (TextView)findViewById(R.id.textView2);
+        lastMsg.setText(user.get(SessionManager.KEY_FNAME) +"  " + user.get(SessionManager.KEY_SURNAME));
     }
 
     @Override
